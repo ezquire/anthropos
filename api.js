6905041f09697fb7fe2b7a2917e3dd3145812a79
ezquire <tyler.gearing@gmail.com>
2019-09-07 14:55:00 -0700

    properly configured .env and created first test user, pat myself on the back

--- api.js
+++ api.js
@@ -1,7 +1,17 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const Synapse = require('synapsenode');
 const Client = Synapse.Client;
+
+// const env = {
+//   client_id: 'client_id_XACzZJLcH0BNnQ10ul9b7kwYGx2Th5EV8mjdrptU',
+//   client_secret: 'client_secret_KUu4ECvzw0VSn51riqo6FjTcQdReGLbZsMOYlB3J',
+//   fingerprint: '08e80b5d3e03516cb2ea76412a232272',
+//   ip_address: '127.0.0.1',
+//   isProduction: false
+// }
+
+// const client = new Client(env);
 
 const client = new Client({
   client_id: process.env.CLIENT_ID,
@@ -11,24 +21,66 @@ const client = new Client({
   isProduction: false
 });
 
-// # createUser or getUser to access User class methods
-const user = client.createUser({
+client.createUser({
   logins: [
     {
-      email: 'test@synapsepay.com'
+      email: 'test@synapsefi.com'
     }
   ],
   phone_numbers: [
-    '901.111.1111'
+    '901.111.1111',
+    'test@synapsefi.com'
   ],
   legal_names: [
     'Test User'
   ],
+  documents: [
+    {
+      email: 'test@test.com',
+      phone_number: '901.111.1111',
+      ip: '::1',
+      name: 'Test User',
+      alias: 'Test',
+      entity_type: 'M',
+      entity_scope: 'Arts & Entertainment',
+      day: 2,
+      month: 5,
+      year: 1989,
+      address_street: '944 Market St.',
+      address_city: 'SF',
+      address_subdivision: 'CA',
+      address_postal_code: '94102',
+      address_country_code: 'US',
+      virtual_docs: [
+        {
+          document_value: '2222',
+          document_type: 'SSN'
+        }
+      ],
+      physical_docs: [
+        {
+          document_value: 'data:image/gif;base64,SUQs==',
+          document_type: 'GOVT_ID'
+        }
+      ],
+      social_docs: [
+        {
+          document_value: 'https://www.facebook.com/valid',
+          document_type: 'FACEBOOK'
+        }
+      ]
+    }
+  ],
   extra: {
-    supp_id: 'my_user_id',
+    supp_id: '122eddfgbeafrfvbbb',
     cip_tag: 1,
     is_business: false
   }
-}, '127.0.0.1');
-
-console.log(user);
+},
+'127.0.0.1'
+)
+.then(( user ) => {
+console.log('user ', user);
+}).catch((e) => {
+  console.log(e);
+});
